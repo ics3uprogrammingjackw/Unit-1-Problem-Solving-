2300a226251b356c71d60f02e4af68ccb26fd3f6
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-02-14 12:50:55 -0500

    Moving Cat Added and Finsihed

---
+++ MovingCatJackW/MovingCatJackW/MovingCatForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MovingCatJackW
+{
+    partial class frmMovingCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.picCat = new System.Windows.Forms.PictureBox();
+            this.mniCat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniCat});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(608, 24);
+            this.mnuStrip.TabIndex = 0;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // picCat
+            // 
+            this.picCat.Image = global::MovingCatJackW.Properties.Resources.cat2;
+            this.picCat.Location = new System.Drawing.Point(237, 99);
+            this.picCat.Name = "picCat";
+            this.picCat.Size = new System.Drawing.Size(148, 118);
+            this.picCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCat.TabIndex = 2;
+            this.picCat.TabStop = false;
+            // 
+            // mniCat
+            // 
+            this.mniCat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniCat1,
+            this.mniCat2});
+            this.mniCat.Name = "mniCat";
+            this.mniCat.Size = new System.Drawing.Size(37, 20);
+            this.mniCat.Text = "Cat";
+            // 
+            // mniCat1
+            // 
+            this.mniCat1.Name = "mniCat1";
+            this.mniCat1.Size = new System.Drawing.Size(152, 22);
+            this.mniCat1.Text = "Cat 1";
+            this.mniCat1.Click += new System.EventHandler(this.mniCat1_Click);
+            // 
+            // mniCat2
+            // 
+            this.mniCat2.Name = "mniCat2";
+            this.mniCat2.Size = new System.Drawing.Size(152, 22);
+            this.mniCat2.Text = "Cat 2";
+            this.mniCat2.Click += new System.EventHandler(this.mniCat2_Click);
+            // 
+            // frmMovingCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 304);
+            this.Controls.Add(this.picCat);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmMovingCat";
+            this.Text = "Moving Cat by Jack W";
+            this.Load += new System.EventHandler(this.frmMovingCat_Load);
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox picCat;
+        private System.Windows.Forms.ToolStripMenuItem mniCat;
+        private System.Windows.Forms.ToolStripMenuItem mniCat1;
+        private System.Windows.Forms.ToolStripMenuItem mniCat2;
+    }
+}
+
---
+++ MovingCatJackW/MovingCatJackW/MovingCatForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by: Jack Wright
+ * Created on: February 14th 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #8 - Moving Cat
+ * This program displays a image of a cat corresponding to the users choice of menu item.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingCatJackW
+{
+    public partial class frmMovingCat : Form
+    {
+        public frmMovingCat()
+        {
+            InitializeComponent();
+        }
+
+        private void mniCat1_Click(object sender, EventArgs e)
+        {
+            this.picCat.Image = Properties.Resources.cat1;
+        }
+
+        private void mniCat2_Click(object sender, EventArgs e)
+        {
+            this.picCat.Image = Properties.Resources.cat2;
+        }
+
+        private void frmMovingCat_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
